0fe344e8941ae03b87940d10442a9e99f3fa83e8
doddyph <d0dd33ph@gmail.com>
2014-01-05 15:18:17 +0700

    update connect to arduino

--- TugasAkhir/gen/com/example/tugasakhir/R.java
+++ TugasAkhir/gen/com/example/tugasakhir/R.java
@@ -24,19 +24,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
+        public static final int action_settings=0x7f08000d;
         public static final int button1=0x7f080005;
+        public static final int button_connect=0x7f080006;
         public static final int checkBox1=0x7f080004;
         public static final int editText1=0x7f080001;
         public static final int editText2=0x7f080003;
         public static final int textView1=0x7f080000;
         public static final int textView2=0x7f080002;
-        public static final int textView3=0x7f080008;
-        public static final int textView4=0x7f08000a;
-        public static final int toggleButton1=0x7f080006;
-        public static final int toggleButton2=0x7f080007;
-        public static final int toggleButton3=0x7f080009;
-        public static final int toggleButton4=0x7f08000b;
+        public static final int textView3=0x7f080009;
+        public static final int textView4=0x7f08000b;
+        public static final int toggleButton1=0x7f080007;
+        public static final int toggleButton2=0x7f080008;
+        public static final int toggleButton3=0x7f08000a;
+        public static final int toggleButton4=0x7f08000c;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -49,6 +50,8 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int label_connect=0x7f05000c;
+        public static final int label_disconnect=0x7f05000d;
         public static final int label_halaman=0x7f05000b;
         public static final int label_kamar_utama_1=0x7f050008;
         public static final int label_kamar_utama_2=0x7f050009;
--- TugasAkhir/src/com/example/tugasakhir/MainActivity.java
+++ TugasAkhir/src/com/example/tugasakhir/MainActivity.java
@@ -1,22 +1,40 @@
 package com.example.tugasakhir;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends Activity implements OnClickListener {
 
+	private Button btnConnect;
 	private TextView txtView1, txtView2, txtView3, txtView4;
 	private ToggleButton btnToggel1, btnToggel2, btnToggel3, btnToggel4;
+	
+	private ConnectionTask connTask;
+	private boolean isConnected = false;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		btnConnect = (Button) findViewById(R.id.button_connect);
+		btnConnect.setOnClickListener(this);
 		
 		txtView1 = (TextView) findViewById(R.id.textView1);
 		txtView2 = (TextView) findViewById(R.id.textView2);
@@ -31,11 +49,21 @@ public class MainActivity extends Activity implements OnClickListener {
 		btnToggel3.setOnClickListener(this);
 		btnToggel4 = (ToggleButton) findViewById(R.id.toggleButton4);
 		btnToggel4.setOnClickListener(this);
+		
+		disableAllToggleButton();
 	}
-
+	
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
+		case R.id.button_connect:
+			if (isConnected) {
+				doDisconnect();
+			}
+			else {
+				doConnect();
+			}
+			break;
 		case R.id.toggleButton1:
 			doToggelButton1();
 			break;
@@ -49,6 +77,28 @@ public class MainActivity extends Activity implements OnClickListener {
 			doToggelButton4();
 			break;
 		}
+	}
+	
+	private void doConnect() {
+		
+	}
+	
+	private void doDisconnect() {
+		
+	}
+	
+	private void disableAllToggleButton() {
+		btnToggel1.setEnabled(false);
+		btnToggel2.setEnabled(false);
+		btnToggel3.setEnabled(false);
+		btnToggel4.setEnabled(false);
+	}
+	
+	private void enableAllToggleButton() {
+		btnToggel1.setEnabled(true);
+		btnToggel2.setEnabled(true);
+		btnToggel3.setEnabled(true);
+		btnToggel4.setEnabled(true);
 	}
 	
 	private void doToggelButton1() {
@@ -102,5 +152,119 @@ public class MainActivity extends Activity implements OnClickListener {
 		
 		Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
 	}
+	
+	private void processCommand(String command) {
+		// TODO
+	}
 
+	private void changeConnectionStatus(boolean isConnected) {
+		Log.v(getClass().getName(), "changeConnectionStatus() isConnected: "+isConnected);
+		this.isConnected = isConnected;
+		
+		if (this.isConnected) {
+			Toast.makeText(getApplicationContext(), 
+					"Successfully connected to server", 
+					Toast.LENGTH_SHORT).show();
+			btnConnect.setText(R.string.label_disconnect);
+			enableAllToggleButton();
+		}
+		else {
+			Toast.makeText(getApplicationContext(), 
+					"Disconnected from server", 
+					Toast.LENGTH_SHORT).show();
+			btnConnect.setText(R.string.label_connect);
+			disableAllToggleButton();
+		}
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		
+		if (connTask != null) {
+			connTask.cancel(true);
+		}
+	}
+	
+	class ConnectionTask extends AsyncTask<Void, byte[], Void> {
+		
+		private Socket mSocket;
+		private InputStream mIn;
+		private OutputStream mOut;
+		private BufferedReader mBufferedReader; // to store the incoming bytes from server
+		
+		private String mIPAddress = "";
+		private int mPort = 8888;
+		private boolean goRun = true;
+		
+		@Override
+		protected Void doInBackground(Void... params) {
+			Log.v(getClass().getName(), "doInBackground()");
+			
+			try {
+				mSocket = new Socket();
+				SocketAddress socketAddress = new InetSocketAddress(mIPAddress, mPort);
+				mSocket.connect(socketAddress, 5*1000);//5 second connection timeout
+				
+				if (mSocket.isConnected()) {
+					changeConnectionStatus(true);
+					
+					mIn = mSocket.getInputStream();
+					mOut = mSocket.getOutputStream();
+					mBufferedReader = new BufferedReader(new InputStreamReader(mIn));
+					
+					while (goRun) {
+						String msgFromServer = mBufferedReader.readLine();
+						byte[] msgBytes = msgFromServer.getBytes();
+						publishProgress(msgBytes);
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				closeConnection();
+			}
+			
+			return null;
+		}
+		
+		@Override
+		protected void onProgressUpdate(byte[]... values) {
+			
+			if (values.length > 0) {
+				String command = new String(values[0]);
+				processCommand(command);
+			}
+		}
+		
+		@Override
+		protected void onCancelled() {
+			Log.v(getClass().getName(), "onCancelled()");
+			goRun = false;
+		}
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			Log.v(getClass().getName(), "onPostExecute()");
+			changeConnectionStatus(false);
+		}
+		
+		public void closeConnection() {
+			Log.v(getClass().getName(), "closeConnection()");
+			
+			try {
+				if (mOut != null) {
+					mOut.close();
+				}
+				if (mIn != null) {
+					mIn.close();
+				}
+				if (mSocket != null) {
+					mSocket.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
